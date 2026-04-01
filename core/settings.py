30ee9a785d289039dcba5db3cac4e22db52e9fdf
q5technologies <eoppong430@gmail.com>
2026-04-01 12:08:54 +0000

    Add CSRF_TRUSTED_ORIGINS for production

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ['office-workplan-production.up.railway.app',
     'localhost', 
     '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://office-workplan-production.up.railway.app'
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
