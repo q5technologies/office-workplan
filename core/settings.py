26d49adc744e7b78224bc38579730ad8c0cf31bc
q5technologies <eoppong430@gmail.com>
2026-04-01 10:31:32 +0000

    Force SSL for database connection

--- core/settings.py
+++ core/settings.py
@@ -91,7 +91,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # <--- Add this line
+    )
 }
 
 
