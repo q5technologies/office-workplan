41d4799b984e8ca28263baca06a2a4e305f382d4
q5technologies <eoppong430@gmail.com>
2026-04-03 09:52:56 +0000

    revised profile

--- users/models.py
+++ users/models.py
@@ -1,9 +1,6 @@
 # users/models.py
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
-from users.models import Profile
 
 class Profile(models.Model):
     USER_ROLES = (('HEAD', 'Head'), ('SUP', 'Supervisor'), ('SUB', 'Subordinate'))
@@ -22,12 +19,3 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        # Default new users to 'SUB' (Subordinate)
-        Profile.objects.create(user=instance, role='SUB')
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
