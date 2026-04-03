4f83a2d1cbff46d0725d753dc1142cd350553f43
q5technologies <eoppong430@gmail.com>
2026-04-03 09:59:50 +0000

    updated profile again

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 # users/models.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -19,3 +21,18 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+        """Automatically creates a Profile whenever a new User is created."""
+        if created:
+            # Defaults to 'SUB' (Subordinate) unless you change it in Admin later
+            Profile.objects.get_or_create(user=instance, defaults={'role': 'SUB'})
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+        """Ensures the profile is saved whenever the user is updated."""
+        try:
+            instance.profile.save()
+        except Profile.DoesNotExist:
+            # Fallback for old users created before this signal was added
+            Profile.objects.create(user=instance, role='SUB')
