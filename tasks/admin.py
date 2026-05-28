ead0c568dd2c14c74d46ac91c8e530c493146fd2
q5technologies <eoppong430@gmail.com>
2026-05-28 09:52:43 +0000

    note filter update

--- tasks/admin.py
+++ tasks/admin.py
@@ -434,7 +434,7 @@ class NoteAdmin(admin.ModelAdmin):
         return False
 
     # --- FILTERS & SEARCH ---
-    list_filter = ('created_at', 'user')
+    list_filter = ('created_at', 'note__user')
     search_fields = ('text', 'user__username', 'task__title')
     date_hierarchy = 'created_at' 
 
